816f79551c08967a5671367647f087ae7d40cd43
Edis MR <edisanthony@gmail.com>
2025-01-02 07:45:58 -0600

    build style improvements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-	app_version_number: '5.5.4',
-	app_version_date: '2024-12-31T15:06:04.504Z',
+	app_version_number: '5.5.5',
+	app_version_date: '2025-01-01T15:06:04.504Z',
 	production: true,
-	buildCounter: '3',
+	buildCounter: '1',
 };
+
