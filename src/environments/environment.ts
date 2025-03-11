0950d3860a76b4cb986726b647f9e202eed7ea14
Edis MR <edisanthony@gmail.com>
2025-03-11 16:40:48 -0600

    build correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-	app_version_number: '5.6',
-	app_version_date: '2025-03-03T15:06:04.504Z',
+	app_version_number: '5.7',
+	app_version_date: '2025-03-11T15:06:04.504Z',
 	production: true,
 	buildCounter: '1',
 };
