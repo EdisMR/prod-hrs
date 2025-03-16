8c44b2a04796c4eb73a66578514bdad0013e51a1
Edis MR <edisanthony@gmail.com>
2025-03-16 11:26:51 -0600

    text corrections

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-	app_version_number: '5.7',
-	app_version_date: '2025-03-11T15:06:04.504Z',
+	app_version_number: '5.7.1',
+	app_version_date: '2025-03-16T15:06:04.504Z',
 	production: false,
 	buildCounter: '1',
 };
